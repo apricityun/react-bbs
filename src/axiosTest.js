c9aca368e98f3afb7ae18a9065294e6d3249805a
apricityun <pang3898@gmail.com>
2022-04-28 13:15:52 +0900

    통신 에러 수정

--- src/axiosTest.js
+++ src/axiosTest.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class AxiosTest extends React.Component {
+    getData = () => {
+        axios
+        // .get('http://localhost:9090/ajax/test?data=test')//STS에서 만들어둔 주소.
+        // .get('http://yts-proxy.now.sh/list_movies.json') //통신성공
+        //package.json에서 proxy 추가 후
+        .get('/ajax/test?data=test')//STS에서 만들어둔 주소.
+        .then((res) =>{
+            console.log('통신성공');
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log('통신실패');
+            console.log(err);
+        });
+    };
 	render(){
         return(
             <div>
-                React 화면 테스트
+                <h1>React 화면 테스트</h1>
+                <button type='button' onClick={this.getData}>
+                    클릭 시 서버와 통신
+                </button>
             </div>
         );
     }
