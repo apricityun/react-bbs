9b7f299c19f0f0057c9e84942921f8b410e15b52
apricityun <pang3898@gmail.com>
2022-04-28 10:55:09 +0900

    axios 사용

---
+++ src/axiosTest.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class AxiosTest extends React.Component {
+	render(){
+        return(
+            <div>
+                React 화면 테스트
+            </div>
+        );
+    }
+}
+export default AxiosTest;
